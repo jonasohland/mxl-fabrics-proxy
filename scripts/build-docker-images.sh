a199923668f7d3d13c5b418990800b83d4006531
Jonas Ohland <jonas.ohland@gmail.com>
2026-03-09 00:57:35 +0100

    update base image, enable wait objects for libfabric

--- scripts/build-docker-images.sh
+++ scripts/build-docker-images.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-base_image="jonasohland/mxl:7df78b1-fabrics"
+base_image="jonasohland/mxl:dea3fcf-fabrics"
 
 project_dir="$(realpath "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"/..)"
 version="$(git -C "${project_dir}" describe --tags)"
